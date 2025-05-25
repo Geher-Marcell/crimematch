2d692212acdd75800510c35da06d88ece5d86ed0
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2025-05-25 20:44:02 +0200

    game logic complete, some fixes are needed

--- src/ts/crimematch.ts
+++ src/ts/crimematch.ts
@@ -1,100 +1,99 @@
 // ez meg nem vegleges, minden valtozhat ha mar megvan az api hivas
 import type Criminal from "./criminal.ts";
-import dataservice from "./dataservice.ts";
 import { GetActiveRandoms } from "./main.ts";
 
+export default class CrimeMatch {
+  criminals: Criminal[] = [];
+  crimeList: string[] = [];
+  constructor() {
+    this.Initialize();
+    document.querySelectorAll(".drop-zone");
+    document
+      .querySelectorAll(".box-rnd")
+      .forEach((el) => el.addEventListener("dragstart", this.dragstartHandler));
+    document.querySelectorAll(".drop-zone").forEach((zone) => {
+      zone.addEventListener("dragover", this.dragoverHandler);
+      zone.addEventListener("drop", this.dropHandler);
+    });
+    document
+      .querySelector("#guess_btn")
+      ?.addEventListener("click", this.CheckMatches.bind(this));
+  }
 
-export default class CrimeMatch{
-    /**
-     *
-     */
-    criminals: Criminal[]= [];
-    crimeList: string[]= [];
-    constructor() {
-        // console.log(activeRandoms)
-        this.Initialize();
-        document.querySelectorAll(".drop-zone")
-      document.querySelectorAll(".box-rnd")
-        .forEach(el => el.addEventListener("dragstart", this.dragstartHandler));
-    document.querySelectorAll(".drop-zone")
-        .forEach(zone => {
-          zone.addEventListener("dragover", this.dragoverHandler);
-          zone.addEventListener("drop", this.dropHandler);
-        });
-      document.querySelector("#guess_btn")?.addEventListener("click", this.CheckMatches.bind(this))
-    }
-    
-    async Initialize(){
-        this.criminals= await GetActiveRandoms();
-        this.DisplayImages();
-        this.RandomizeCrimes();
-    }
+  async Initialize() {
+    this.criminals = await GetActiveRandoms();
+    this.DisplayImages();
+    this.RandomizeCrimes();
+  }
 
-    
-
-    RandomizeCrimes(){
-        const crimes = document.querySelectorAll(".box-rnd") as NodeListOf<HTMLDivElement>;
-        this.criminals.forEach(c => {
-            this.crimeList.push(c.crime);
-        })
-        const shuffledCrimes = this.crimeList.sort((a, b) => 0.5 - Math.random());
-        for(let i = 0; i < crimes.length; i++){
-            // console.log(shuffledCrimes[i]);
-            crimes[i].innerHTML = `<p >${shuffledCrimes[i]}</p>`;
-        }
+  RandomizeCrimes() {
+    const crimes = document.querySelectorAll(
+      ".box-rnd"
+    ) as NodeListOf<HTMLDivElement>;
+    this.criminals.forEach((c) => {
+      this.crimeList.push(c.crime);
+    });
+    const shuffledCrimes = this.crimeList.sort((a, b) => 0.5 - Math.random());
+    for (let i = 0; i < crimes.length; i++) {
+      crimes[i].innerHTML = `<p >${shuffledCrimes[i]}</p>`;
     }
+  }
 
-    DisplayImages(){
-        const imgs = document.querySelectorAll(".image img") as NodeListOf<HTMLImageElement>;
-        console.log("képek: " + imgs);
-        console.log("faszok: " + this.criminals)
-        for(let i = 0; i < imgs.length; i++){
-            console.log("Aktuális fasz: " + this.criminals[i])
-            console.log("faszindex: " + i)
-            imgs[i].src = this.criminals[i].img
-        }
+  DisplayImages() {
+    const imgs = document.querySelectorAll(
+      ".image img"
+    ) as NodeListOf<HTMLImageElement>;
+    console.log("képek: " + imgs);
+    console.log("faszok: " + this.criminals);
+    for (let i = 0; i < imgs.length; i++) {
+      console.log("Aktuális fasz: " + this.criminals[i]);
+      console.log("faszindex: " + i);
+      imgs[i].src = this.criminals[i].img;
     }
-    dragstartHandler(e:any) {
+  }
+  dragstartHandler(e: any) {
     e.dataTransfer.setData("text/plain", e.target.id);
   }
-  dragoverHandler(e:any) {
+  dragoverHandler(e: any) {
     e.preventDefault();
   }
-  dropHandler(e:any) {
+  dropHandler(e: any) {
     e.preventDefault();
-    
+
     const id = e.dataTransfer.getData("text/plain");
     // console.log( e.currentTarget);
     const dragged = document.getElementById(id);
-    if(e.currentTarget.children.length == 0){
-
+    if (e.currentTarget.children.length == 0) {
       // console.log(dragged)
       if (dragged) {
         e.currentTarget.appendChild(dragged);
-      } 
-    } 
-  }
-  
-  CheckMatches(){
-    const zones: NodeListOf<HTMLDivElement> = document.querySelectorAll(".guess-zone")
-    for(let i = 0; i < zones.length; i++){
-      // console.log(zones[i]);
-      if(zones[i].children[0] != undefined){
-        
-        let desc: string = (zones[i].children[0]).children[0].innerHTML;
-        this.CheckCorrection(desc, i);
-        // console.log((zones[i].children[0]).children[0].innerHTML)
       }
     }
-  };
-
-  CheckCorrection(d: string, i: number){
-    const description = this.criminals[i].crime.trim().toLowerCase();
-    const descGuess = d.trim().toLowerCase();
-
-    if(descGuess === description) console.log(`correct ${i}: ${description}-->${descGuess}`);
-    else console.log(`incorrect ${i}: ${description}-->${descGuess}`);
   }
 
+  CheckMatches() {
+    let matchCount: number = 0;
+    const zones: NodeListOf<HTMLDivElement> =
+      document.querySelectorAll(".guess-zone");
+    for (let i = 0; i < zones.length; i++) {
+      if (zones[i].children[0] != undefined) {
+        let desc: string = zones[i].children[0].children[0].innerHTML
+          .trim()
+          .toLowerCase();
+        const description = this.criminals[i].crime.trim().toLowerCase();
 
+        if (description == desc) {
+          console.log(`correct ${i}: ${description}-->${desc}`);
+          zones[i].style.backgroundColor = "green";
+        } else {
+          console.log(`incorrect ${i}: ${description}-->${desc}`);
+          zones[i].style.backgroundColor = "red";
+        }
+      }
+    }
+    if (matchCount != 5) return;
+    console.log("yay");
+    (document.querySelector("#msg-div") as HTMLDivElement).innerHTML =
+      "<h3 class='text-center'>Your matches are correct!</h3><br><a href='' class='text-4xl bg-blue-900 rounded-2xl px-6 py-2 font-bold hover:text-gray-400'>New challange</a>";
+  }
 }
